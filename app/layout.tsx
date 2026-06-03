f5386ae0cfafbb11154a010ae67500ed627c8902
thomasjbartley-creator <thomasjbartley@gmail.com>
2026-06-03 11:15:04 -0500

    Add Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import GoogleTranslate from "@/components/GoogleTranslate";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
         />
         <GoogleTranslate />
         {children}
+        <Analytics />
       </body>
     </html>
   );
